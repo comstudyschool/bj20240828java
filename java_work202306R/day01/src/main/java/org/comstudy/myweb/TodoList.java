f94c003472aba8e77ea2077d900f2e5508637254
comstudyschool <comstudychool@gmail.com>
2024-08-29 15:22:07 +0900

    jsp day01

--- java_work202306R/day01/src/main/java/org/comstudy/myweb/TodoList.java
+++ java_work202306R/day01/src/main/java/org/comstudy/myweb/TodoList.java
@@ -15,7 +15,7 @@ public class TodoList extends HttpServlet {
 
 	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		// TodoList.jsp페이지로 forward 하기
-		// WEB-INF 내부는 외부에서 브라우저로 접근 할 수 없다. (내부 클래스에서 접근 하거나 forward 가능)
+		// WEB-INF 내부는 외부에서 브라우저로 접근 할 수 없다. (내부 클래스에서 접근 하거나 forward 가능) 
 		String path = "/WEB-INF/views/TodoList.jsp";
 		RequestDispatcher view =  req.getRequestDispatcher(path);
 		view.forward(req, res);
